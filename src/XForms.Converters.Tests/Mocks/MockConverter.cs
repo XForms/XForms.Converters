d87610d0f05463b141578fc5c39c367e758c91b4
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2016-02-27 22:09:49 +0300

    Added tests for ValueConverterGroup

---
+++ src/XForms.Converters.Tests/Mocks/MockConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace XForms.Converters.Tests.Mocks
+{
+	public class MockConverter : IValueConverter
+	{
+		public const string Tag = "Mock";
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return Tag + value.ToString();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ src/XForms.Converters.Tests/ValueConverterGroupTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using NUnit.Framework;
+using XForms.Converters.Tests.Mocks;
+
+namespace XForms.Converters.Tests
+{
+	[TestFixture]
+	public class ValueConverterGroupTests
+	{
+		[TestCase("Value")]
+		public void ConvertTest(object value)
+		{
+			var converterGroup = new ValueConverterGroup();
+
+			converterGroup.Add(new MockConverter());
+			converterGroup.Add(new MockConverter());
+			converterGroup.Add(new MockConverter());
+
+			var result = converterGroup.Convert(value, typeof(string), null, CultureInfo.CurrentCulture);
+
+			var correctValue = $"{MockConverter.Tag}{MockConverter.Tag}{MockConverter.Tag}{value.ToString()}";
+
+			Assert.AreEqual(correctValue, result);
+		}
+	}
+}
